135c237d43849a42fc47723008120d835c029476
Hamza Jadid <hamzah.jadeed@gmail.com>
2021-07-19 22:57:53 +0300

    Update isLandline.js

--- phone/isLandline.js
+++ phone/isLandline.js
@@ -1,5 +1,5 @@
 function isLandline(number) {
-  const pattern = /^(00961|\+961|961)?0?(1|3|4|5|6|8|9)[0-9]{6}$/;
+  const pattern = /^(00961|\+961|961)?0?(1|4|5|6|7|8|9)[0-9]{6}$/;
   return pattern.test(number);
 }
 
